06cca283bc89c34b7302b3f3dd7ae6ab17a0bbca
Pickens, Brian M <brian.m.pickens@gmail.com>
2019-06-16 20:13:03 -0500

    - Call TF.exe to get local workspaces

--- AzureDevops.Services.Test/WorkspaceServiceTests.cs
+++ AzureDevops.Services.Test/WorkspaceServiceTests.cs
@@ -1,19 +1,44 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Xunit;
-using AzureDevops.Services;
+using Xunit.Abstractions;
 
 namespace AzureDevops.Services.Test
 {
     public class WorkspaceServiceTests
     {
 
-        [Fact]
-        public void Test_GetListOfWorkspaceServices()
+        private readonly ITestOutputHelper _output;
+
+        public WorkspaceServiceTests(ITestOutputHelper output)
         {
-            var workspaceService = new WorkspaceService();
-            Assert.True(false);
+            this._output = output;
         }
+
+        [Fact]
+        public async Task Test_GetListOfWorkspaceServices()
+        {
+            var workspaceService = new WorkspaceService(new Uri("http://chtfs1:8080/tfs/defaultcollection"));
+            var workspaces = await workspaceService.GetWorkspaces();
+            Assert.IsType<Workspaces>(workspaces);
+            Assert.NotEmpty(workspaces.Items);
+
+            foreach (var workspace in workspaces.Items)
+            {
+                _output.WriteLine($"workspace: {workspace.name} | owner: {workspace.owner}");
+            }
+
+        }
+
+//        [Fact]
+//        public void Test()
+//        {
+//            var foo = Process.Start(new ProcessStartInfo()
+//            {
+//                FileName = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Professional\\Common7\\IDE\\CommonExtensions\\Microsoft\\TeamFoundation\\Team Explorer\\TF.exe"
+//            })
+//        }
 
     }
 }
--- AzureDevops.Services/WorkspaceService.cs
+++ AzureDevops.Services/WorkspaceService.cs
@@ -1,8 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace AzureDevops.Services
 {
+
     public class WorkspaceService
     {
+        private const string TFPATH =
+            "Common7\\IDE\\CommonExtensions\\Microsoft\\TeamFoundation\\Team Explorer\\TF.exe";
+
+        private readonly Uri _collection;
+
+        public WorkspaceService(Uri collection)
+        {
+            _collection = collection;
+        }
+
+        public async Task<Workspaces> GetWorkspaces()
+        {
+            var x = new XmlSerializer(typeof(Workspaces));
+
+            // Use vswhere.exe to locate lastest copy of installed VS instance
+            var vsInstallPathProcess = new AsyncProcess("vswhere.exe", @"-prerelease -latest -property installationPath");
+            var vsInstallPath = await vsInstallPathProcess.Start().Output().FirstAsync().ConfigureAwait(false);
+
+            // Combine install path with relative TF.exe path
+            var tfFullPath = Path.Combine(vsInstallPath, TFPATH);
+
+            // Execute TF workspaces command
+            var xmlLineArrayProcess = new AsyncProcess(tfFullPath, $@"workspaces /format:xml /collection:{_collection}").Start();
+            var error = string.Join(Environment.NewLine, await xmlLineArrayProcess.Error().ToArrayAsync().ConfigureAwait(false));
+            var output = string.Join(Environment.NewLine, await xmlLineArrayProcess.Output().ToArrayAsync().ConfigureAwait(false));
+
+            await xmlLineArrayProcess.WaitForExitCodeAsync().ConfigureAwait(false);
+
+            if (error.Length > 0)
+            {
+                throw new ApplicationException(error);
+            }
+
+            using var stream = new StringReader(output);
+            return x.Deserialize(stream) as Workspaces;
+        }
+
+        public async Task Delete(WorkspacesWorkspace workspace)
+        {
+
+        }
     }
+
 }
